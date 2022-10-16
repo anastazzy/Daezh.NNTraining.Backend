b89b6ca0d1c1eafe2df15dde87cca7fa778fcbab
Anastasia <shitovanap@gmail.com>
2022-10-16 23:59:05 +0300

    in progress

---
+++ src/NNTraining.Domain/Tools/CustomModelParametersConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace NNTraining.Domain.Tools;
+
+public class CustomModelParametersConverter : JsonConverter<NNParameters>
+{
+    public override NNParameters? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(Utf8JsonWriter writer, NNParameters value, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+}
